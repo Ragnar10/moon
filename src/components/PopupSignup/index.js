dc00740812a341708812ba469892722985416571
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-06-23 01:12:24 +0300

    add twitter(test)

--- src/components/PopupSignup/index.js
+++ src/components/PopupSignup/index.js
@@ -1,5 +1,8 @@
 // Core
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// Instruments
+import queryString from 'query-string';
 // Utils
 import { cutLine } from '../../utils';
 // Actions
@@ -21,6 +24,63 @@ const PopupSignup = () => {
         dispatch(setTelegramData(res));
     };
 
+    const apiPath = process.env.REACT_APP_SERVERLESS;
+
+    const login = () => {
+        (async () => {
+            try {
+                // OAuth Step 1
+                const response = await fetch(`${apiPath}/twitter/oauth/request_token`, {
+                    method:  'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                const { oauth_token } = response.data;
+                // Oauth Step 2
+                window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_token}`;
+            } catch (errors) {
+                console.error(errors);
+            }
+        })();
+    };
+
+    useEffect(() => {
+        (async () => {
+            const { oauth_token, oauth_verifier } = queryString.parse(window.location.search);
+
+            if (oauth_token && oauth_verifier) {
+                try {
+                    // Oauth Step 3
+                    await fetch(`${apiPath}/twitter/oauth/access_token`, {
+                        method:  'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: { oauth_token, oauth_verifier },
+                    });
+                } catch (errors) {
+                    console.error(errors);
+                }
+            }
+
+            try {
+                // Authenticated Resource Access
+                const data = await fetch(`${apiPath}/twitter/users/profile_banner`, {
+                    method:  'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                console.log(data);
+            } catch (errors) {
+                console.error(errors);
+            }
+        })();
+    }, []);
+
     return (
         <section className = { Styles.popup }>
             <div className = { Styles.shadow } />
@@ -33,7 +93,9 @@ const PopupSignup = () => {
                     { 'To be eligible for the Bonus connect your twitter and telegram to verify you are a real Human. Collect social points for the free IDO by being active on Telegram and Twitter.' }
                 </p>
                 <div className = { Styles.content_btns }>
-                    <button className = { Styles.twitter_btn }>
+                    <button
+                        onClick = { () => login() }
+                        className = { Styles.twitter_btn }>
                         <span />
                         <span>{ 'Twitter' }</span>
                     </button>
