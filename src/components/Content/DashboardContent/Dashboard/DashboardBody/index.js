32931440a0a187aa90de10d6cf032777dcd4b6b5
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-07-21 23:51:45 +0300

    fix

--- src/components/Content/DashboardContent/Dashboard/DashboardBody/index.js
+++ src/components/Content/DashboardContent/Dashboard/DashboardBody/index.js
@@ -1,228 +1,231 @@
 // Core
-import React, { useState } from "react";
-//Styles
-import Styles from "./styles.module.scss";
+import { useState } from 'react';
 // Mui
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Pagination, Stack } from "@mui/material";
-import DashboardHead from "../DashboardHead";
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Pagination, Stack } from '@mui/material';
+import DashboardHead from '../DashboardHead';
+// Styles
+import Styles from './styles.module.scss';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#252F45",
-    color: "#AEC0CA",
-    fontSize: 14,
-    border: "none",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
+    [ `&.${tableCellClasses.head}` ]: {
+        backgroundColor: '#252F45',
+        color:           '#AEC0CA',
+        fontSize:        14,
+        border:          'none',
+    },
+    [ `&.${tableCellClasses.body}` ]: {
+        fontSize: 14,
+    },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: "#374259",
-  },
+    '&:nth-of-type(odd)': {
+        backgroundColor: '#374259',
+    },
 
-  "&:nth-of-type(2n)": {
-    backgroundColor: "#333E54",
-  },
+    '&:nth-of-type(2n)': {
+        backgroundColor: '#333E54',
+    },
 
-  th: {
-    color: "#FFFFFF !important",
-  },
+    th: {
+        color: '#FFFFFF !important',
+    },
 
-  "td, th": {
-    border: "none",
-    color: "#AEC0CA",
-    fontWeight: 700,
-    lineHeight: "14px",
-  },
+    'td, th': {
+        border:     'none',
+        color:      '#AEC0CA',
+        fontWeight: 700,
+        lineHeight: '14px',
+    },
 
-  "& td:nth-of-type(1)": {
-    color: "#FFFFFF !important",
-    lineHeight: "20px",
-    fontWeight: "400 !important",
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
+    '& td:nth-of-type(1)': {
+        color:      '#FFFFFF !important',
+        lineHeight: '20px',
+        fontWeight: '400 !important',
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
 }));
 
 function createData(userId, address, farming, total, active, closed, earned) {
-  return { userId, address, farming, total, active, closed, earned };
+    return {
+        userId, address, farming, total, active, closed, earned,
+    };
 }
 
 const rows = [
-  createData(
-    "#0000001",
-    "0xb794f5ea0ba39494ce839613fffba74279579268",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A"
-  ),
-  createData(
-    "#0000002",
-    "0xb794f5ea0ba39494ce839613fffba74279579268",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A"
-  ),
-  createData(
-    "#0000003",
-    "0xb794f5ea0ba39494ce839613fffba74279579268",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A"
-  ),
-  createData(
-    "#0000004",
-    "0xb794f5ea0ba39494ce839613fffba74279579268",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A"
-  ),
-  createData(
-    "#0000005",
-    "0xb794f5ea0ba39494ce839613fffba74279579268",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A",
-    "N/A"
-  ),
+    createData(
+        '#0000001',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+    ),
+    createData(
+        '#0000002',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+    ),
+    createData(
+        '#0000003',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+    ),
+    createData(
+        '#0000004',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+    ),
+    createData(
+        '#0000005',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+        'N/A',
+    ),
 ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
 
 const Dashboard = () => {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const [order, setOrder] = useState("asc");
-  const [orderBy, setOrderBy] = useState("calories");
-  const [selected, setSelected] = React.useState([]);
-  const [dense, setDense] = React.useState(false);
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('calories');
+    const [selected, setSelected] = useState([]);
+    const [dense, setDense] = useState(false);
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = rows.map((n) => n.name);
+            setSelected(newSelecteds);
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
+            return;
+        }
+        setSelected([]);
+    };
 
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
 
-    setSelected(newSelected);
-  };
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
 
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
+        setSelected(newSelected);
+    };
 
-  const isSelected = (name) => selected.indexOf(name) !== -1;
+    const handleChangeDense = (event) => {
+        setDense(event.target.checked);
+    };
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    const isSelected = (name) => selected.indexOf(name) !== -1;
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows
+    = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
 
-  return (
-    <section>
-      <h4 className={Styles.title}>Recent Signups</h4>
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 800 }} aria-label="customized table">
-          <DashboardHead
-            numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={rows.length}
-          />
-          <TableBody>
-            {(rowsPerPage > 0
-              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : rows
-            ).map((row) => (
-              <StyledTableRow key={row.userId}>
-                <StyledTableCell component="th" scope="row">
-                  {row.userId}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.address}</StyledTableCell>
-                <StyledTableCell align="right">{row.farming}</StyledTableCell>
-                <StyledTableCell align="right">{row.total}</StyledTableCell>
-                <StyledTableCell align="right">{row.active}</StyledTableCell>
-                <StyledTableCell align="right">{row.closed}</StyledTableCell>
-                <StyledTableCell align="right">{row.earned}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-            {emptyRows > 0 && (
-              <StyledTableRow style={{ height: 53 * emptyRows }}>
-                <StyledTableCell colSpan={7} />
-              </StyledTableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Stack mt={2} justifyContent="center" alignItems="center">
-        <Pagination
-          className={Styles.pagination}
-          count={+(rows.length / 2).toFixed()}
-          shape="rounded"
-          variant="outlined"
-          page={page}
-          defaultPage={0}
-          onChange={handleChangePage}
-        />
-      </Stack>
-    </section>
-  );
+    return (
+        <section>
+            <h4 className = { Styles.title }>Recent Signups</h4>
+
+            <TableContainer component = { Paper }>
+                <Table sx = { { minWidth: 800 } } aria-label = 'customized table'>
+                    <DashboardHead
+                        numSelected = { selected.length }
+                        order = { order }
+                        orderBy = { orderBy }
+                        onSelectAllClick = { handleSelectAllClick }
+                        onRequestSort = { handleRequestSort }
+                        rowCount = { rows.length } />
+                    <TableBody>
+                        { (rowsPerPage > 0
+                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rows
+                        ).map((row) => (
+                            <StyledTableRow key = { row.userId }>
+                                <StyledTableCell component = 'th' scope = 'row'>
+                                    { row.userId }
+                                </StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.address }</StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.farming }</StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.total }</StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.active }</StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.closed }</StyledTableCell>
+                                <StyledTableCell align = 'right'>{ row.earned }</StyledTableCell>
+                            </StyledTableRow>
+                        )) }
+                        { emptyRows > 0 && (
+                            <StyledTableRow style = { { height: 53 * emptyRows } }>
+                                <StyledTableCell colSpan = { 7 } />
+                            </StyledTableRow>
+                        ) }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Stack
+                mt = { 2 } justifyContent = 'center'
+                alignItems = 'center'>
+                <Pagination
+                    className = { Styles.pagination }
+                    count = { Number((rows.length / 2).toFixed()) }
+                    shape = 'rounded'
+                    variant = 'outlined'
+                    page = { page }
+                    defaultPage = { 0 }
+                    onChange = { handleChangePage } />
+            </Stack>
+        </section>
+    );
 };
 
 export default Dashboard;
--- src/components/Content/DashboardContent/Dashboard/DashboardHead/index.js
+++ src/components/Content/DashboardContent/Dashboard/DashboardHead/index.js
@@ -1,92 +1,92 @@
-import React from 'react';
-
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { styled } from "@mui/material/styles";
+// Mui
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { styled } from '@mui/material/styles';
 import PropTypes from 'prop-types';
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import { Box, TableSortLabel } from '@mui/material';
 import { visuallyHidden } from '@mui/utils';
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: "#252F45",
-      color: "#AEC0CA",
-      fontSize: 14,
-      border: "none",
+    [ `&.${tableCellClasses.head}` ]: {
+        backgroundColor: '#252F45',
+        color:           '#AEC0CA',
+        fontSize:        14,
+        border:          'none',
     },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
+    [ `&.${tableCellClasses.body}` ]: {
+        fontSize: 14,
     },
-  }));
+}));
 
 
 const headCells = [
     {
-      id: 'User',
-      numeric: false,
-      disablePadding: true,
-      label: 'User ID',
+        id:             'User',
+        numeric:        false,
+        disablePadding: true,
+        label:          'User ID',
     },
     {
-      id: 'Address',
-      numeric: false,
-      disablePadding: true,
-      label: 'Address',
+        id:             'Address',
+        numeric:        false,
+        disablePadding: true,
+        label:          'Address',
     },
     {
-      id: 'Farming',
-      numeric: true,
-      disablePadding: false,
-      label: 'Farming Pos.', 
+        id:             'Farming',
+        numeric:        true,
+        disablePadding: false,
+        label:          'Farming Pos.',
     },
     {
-      id: 'Total',
-      numeric: true,
-      disablePadding: false,
-      label: 'Total Pos.',
+        id:             'Total',
+        numeric:        true,
+        disablePadding: false,
+        label:          'Total Pos.',
     },
     {
-      id: 'Active',
-      numeric: true,
-      disablePadding: false,
-      label: 'Active Pos.',
+        id:             'Active',
+        numeric:        true,
+        disablePadding: false,
+        label:          'Active Pos.',
     },
     {
-      id: 'Closed',
-      numeric: true,
-      disablePadding: false,
-      label: 'Closed Pos.',
+        id:             'Closed',
+        numeric:        true,
+        disablePadding: false,
+        label:          'Closed Pos.',
     },
     {
-      id: 'Earned',
-      numeric: true,
-      disablePadding: false,
-      label: 'Earned Com.',
+        id:             'Earned',
+        numeric:        true,
+        disablePadding: false,
+        label:          'Earned Com.',
     },
-  ];
+];
 
 const DashboardHead = (props) => {
-    const {  order, orderBy, numSelected, rowCount, onRequestSort } =
-      props;
+    const {
+        order, orderBy, numSelected, rowCount, onRequestSort,
+    }
+      = props;
     const createSortHandler = (property) => (event) => {
-      onRequestSort(event, property);
+        onRequestSort(event, property);
     };
 
-  return (
-    <TableHead>
-        <TableRow>
-        {headCells.map((headCell) => (
-            <StyledTableCell
-                key={headCell.id}
-                align={headCell.id === 'User' ? 'center' : 'left'}
-                padding={headCell.disablePadding ? 'none' : 'normal'}
-                sortDirection={orderBy === headCell.id ? order : false}
-            >
-                {headCell.label}
+    return (
+        <TableHead>
+            <TableRow>
+                { headCells.map((headCell) => (
+                    <StyledTableCell
+                        key = { headCell.id }
+                        align = { headCell.id === 'User' ? 'center' : 'left' }
+                        padding = { headCell.disablePadding ? 'none' : 'normal' }
+                        sortDirection = { orderBy === headCell.id ? order : false }>
+                        { headCell.label }
 
-                {/* <TableSortLabel
+                        { /* <TableSortLabel
                 active={orderBy === headCell.id}
                 direction={orderBy === headCell.id ? order : 'asc'}
                 onClick={createSortHandler(headCell.id)}
@@ -96,20 +96,20 @@ const DashboardHead = (props) => {
                     {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                   </Box>
                 ) : null}
-              </TableSortLabel> */}
-            </StyledTableCell>
-        ))}
-        </TableRow>
-    </TableHead>
-  )
-}
+              </TableSortLabel> */ }
+                    </StyledTableCell>
+                )) }
+            </TableRow>
+        </TableHead>
+    );
+};
 export default DashboardHead;
 
 DashboardHead.propTypes = {
-    numSelected: PropTypes.number.isRequired,
+    numSelected:   PropTypes.number.isRequired,
     onRequestSort: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-  };
+    order:         PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy:       PropTypes.string.isRequired,
+    rowCount:      PropTypes.number.isRequired,
+};
 
