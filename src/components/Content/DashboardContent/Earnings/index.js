2edd15673f365e8520937c1a365dcab7c4a44263
Гудов Олег Русланович <oleg.gudov@privatbank.ua>
2022-07-28 15:40:57 +0300

    fix users count

--- src/components/Content/DashboardContent/Earnings/index.js
+++ src/components/Content/DashboardContent/Earnings/index.js
@@ -1,85 +1,84 @@
-import React from "react";
-//Styles
-import Styles from "./styles.module.scss";
+// Styles
+import Styles from './styles.module.scss';
 
 
 const Earnings = () => {
-  return (
-    <section className={Styles.wrapper}>
-        <div className={Styles.flex_sb}>
-            <div className={Styles.title}>
-                <p>Withdrawable earnings</p>
-                <p>Funds ready for withdraw</p>
+    return (
+        <section className = { Styles.wrapper }>
+            <div className = { Styles.flex_sb }>
+                <div className = { Styles.title }>
+                    <p>Withdrawable earnings</p>
+                    <p>Funds ready for withdraw</p>
+                </div>
+
+                <button className = { Styles.refresh_btn }>
+                    <span />
+                </button>
             </div>
 
-            <button className={Styles.refresh_btn}>
+            <div className = { Styles.bit_coin }>
                 <span />
-            </button>
-        </div>
+                <p>5.0000025</p>
+                <p>BTC</p>
+            </div>
 
-        <div className={Styles.bit_coin}>
-            <span />
-            <p>5.0000025</p>
-            <p>BTC</p>
-        </div>
+            <div className = { Styles.flex_sb }>
+                <div>
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.eth_icon}` } />
+                        <p>0.0000</p>
+                        <p>ETH</p>
+                    </div>
 
-        <div className={Styles.flex_sb}>
-            <div>
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.eth_icon}`} />
-                    <p>0.0000</p>
-                    <p>ETH</p>
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.trx_icon}` } />
+                        <p>0.0000</p>
+                        <p>TRX</p>
+                    </div>
+
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.bnb_icon}` } />
+                        <p>0.0000</p>
+                        <p>BNB</p>
+                    </div>
+
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.cake_icon}` } />
+                        <p>0.0000</p>
+                        <p>CAKE</p>
+                    </div>
                 </div>
 
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.trx_icon}`} />
-                    <p>0.0000</p>
-                    <p>TRX</p>
-                </div>
+                <div>
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.busd_icon}` } />
+                        <p>0.0000</p>
+                        <p>BUSD</p>
+                    </div>
 
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.bnb_icon}`} />
-                    <p>0.0000</p>
-                    <p>BNB</p>
-                </div>
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.usdt_icon}` } />
+                        <p>0.0000</p>
+                        <p>USDT</p>
+                    </div>
 
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.cake_icon}`} />
-                    <p>0.0000</p>
-                    <p>CAKE</p>
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.bnb_icon}` } />
+                        <p>0.0000</p>
+                        <p>BNB</p>
+                    </div>
+
+                    <div className = { Styles.coin }>
+                        <span className = { `${Styles.icon} ${Styles.cake_icon}` } />
+                        <p>0.0000</p>
+                        <p>CAKE</p>
+                    </div>
                 </div>
             </div>
 
-            <div>
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.busd_icon}`} />
-                    <p>0.0000</p>
-                    <p>BUSD</p>
-                </div>
-
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.usdt_icon}`} />
-                    <p>0.0000</p>
-                    <p>USDT</p>
-                </div>
-
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.bnb_icon}`} />
-                    <p>0.0000</p>
-                    <p>BNB</p>
-                </div>
-
-                <div className={Styles.coin}>
-                    <span className={`${Styles.icon} ${Styles.cake_icon}`} />
-                    <p>0.0000</p>
-                    <p>CAKE</p>
-                </div>
-            </div>
-        </div>
-
-        <button className={Styles.withdraw_btn}>Withdraw</button>
-    </section>
-  );
+            <button className = { Styles.withdraw_btn }>Withdraw</button>
+        </section>
+    );
 };
 
 export default Earnings;
